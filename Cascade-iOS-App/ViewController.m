271e668d4384b0b190a2098e1af54299e40fd958
XinweiUW <liuxinw@uw.edu>
2015-03-25 18:41:23 -0700

    Initial Commit

---
+++ Cascade-iOS-App/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Cascade-iOS-App
+//
+//  Created by iGuest on 3/25/15.
+//  Copyright (c) 2015 Cascade. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
