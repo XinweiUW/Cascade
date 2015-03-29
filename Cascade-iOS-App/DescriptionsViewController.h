df2c2c321091a31259b7670c15cca93c66ef5c3d
XinweiUW <liuxinw@uw.edu>
2015-03-28 22:42:47 -0700

    storyboard changed

---
+++ Cascade-iOS-App/DescriptionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DescriptionsViewController.h
+//  Cascade-iOS-App
+//
+//  Created by iGuest on 3/28/15.
+//  Copyright (c) 2015 Cascade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DescriptionsViewController : UIViewController
+
+@end
---
+++ Cascade-iOS-App/LandingTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LandingTableViewController.h
+//  Cascade-iOS-App
+//
+//  Created by iGuest on 3/28/15.
+//  Copyright (c) 2015 Cascade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LandingTableViewController : UITableViewController
+
+@end
