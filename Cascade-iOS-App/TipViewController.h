d1652baae032a7e230c586ed86390b876580f81c
dwill008 <reinhart046@gmail.com>
2015-04-06 21:44:13 -0700

    Added view controllers for about, settings and safety tips

---
+++ Cascade-iOS-App/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  Cascade-iOS-App
+//
+//  Created by iGuest on 4/6/15.
+//  Copyright (c) 2015 Cascade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
