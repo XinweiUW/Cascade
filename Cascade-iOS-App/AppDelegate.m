ce89bb95cd97dc81edad9f9f6e6e5ce040e91b49
dwill008 <reinhart046@gmail.com>
2015-05-12 17:14:16 -0700

    Changed landing screen time to 1.5 seconds

--- Cascade-iOS-App/AppDelegate.m
+++ Cascade-iOS-App/AppDelegate.m
@@ -25,7 +25,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    sleep(1);
+    sleep(1.5);
     // Override point for customization after application launch.
     //[[UINavigationBar appearance] setBarTintColor:[UIColor greenColor]];
     //[[UINavigationBar appearance] setBarTintColor:UIColorFromRGB(0x66CC00)];
